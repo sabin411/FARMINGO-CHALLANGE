7532c345b3582553ff3634a3ec9f067f25b199de
iamsabinstha <sherasabin411@gmail.com>
2021-10-15 00:33:05 +0545

    ✨ FIX #1 wrapped _app js file by Layout comp

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import styles from "../styles/Home.module.css";
+import AboutFarmingo from "../components/homeSections/GoodAboutFarmingo";
+import Head from "next/head";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <section className={styles.container}>
+        <AboutFarmingo />
+        ABOUT SECTION
+      </section>
+    </>
+  );
+}
