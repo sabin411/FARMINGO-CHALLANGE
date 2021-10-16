a78a2cddd045241bdc9c1cf9f37412eb0aa28c84
iamsabinstha <sherasabin411@gmail.com>
2021-10-16 10:35:28 +0545

    ✨ FIX #8 Added category section in home page.

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import styles from "../styles/Home.module.css";
 import AboutFarmingo from "../components/homeSections/GoodAboutFarmingo";
 import Head from "next/head";
+import CategoriesSec from "../components/homeSections/CategoriesSec";
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
-      <section className={styles.container}>
+      <section className={styles.homeSections_container}>
         <AboutFarmingo />
-        ADDED ABOUT SECTION
+        <CategoriesSec />
       </section>
     </>
   );
